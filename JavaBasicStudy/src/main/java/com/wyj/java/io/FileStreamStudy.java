af409fca8c363148ea7d8bff615342753b3775e8
王永杰 <xiayuexingkong@gmail.com>
2024-05-19 22:44:55 +0800

    File文件输出流读取文件内容。

---
+++ JavaBasicStudy/src/main/java/com/wyj/java/io/FileStreamStudy.java
@@ -0,0 +1,30 @@
+package com.wyj.java.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileStreamStudy {
+
+    public static void main(String[] args) {
+
+        // 通过IO流读取Animal文件的内容
+        String path = "Java基础.md";
+        File file = new File(path);
+        System.out.println("absolute path:" + file.getAbsolutePath());
+        String filePath = "D:\\study\\Java\\github\\JavaTutorials\\JavaBasicStudy\\src" +
+                "\\main\\java\\com\\wyj\\java\\io\\Animal.java";
+        File file1 = new File(filePath);
+        System.out.println("directoryFile exist:" + file1.exists());
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+
+    }
+}
